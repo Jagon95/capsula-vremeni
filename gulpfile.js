2cf748c87cb64cbeee098764300ca79579b9bdae
Barrel <jagon95@gmail.com>
2017-12-29 01:52:40 +0300

    + compute image sizes + improve thumbnails creating

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,9 @@ const data = require('gulp-data');
 const copy = require('gulp-copy');
 const env = require('gulp-environments');
 const imageSize = require('image-size-export');
+const uncss = require('gulp-uncss');
+const sequence = require('gulp-sequence');
+const mkPromise = require('gulp-stream-to-promise');
 
 const dev = env.development;
 const prod = env.production;
@@ -77,6 +80,9 @@ gulp.task('build-css', ['sass'], () => {
         // .pipe(cleanCSS({
         //     level: 2
         // }))
+        .pipe(prod(uncss({
+            html: 'http://localhost:1488'
+        })))
         .pipe(prod(autoprefixer({browsers: '> 0.3%'})))
         .pipe(gulp.dest('build/css'))
         .pipe(dev(connect.reload()));
@@ -93,7 +99,7 @@ gulp.task('purify-css', () => {
         'libs/bootstrap/dist/css/bootstrap.css',
         'libs/semantic/dist/semantic.css'
     ])
-        .pipe(purify(['index.html', 'js/main.js']))
+        .pipe(purify(['index.html', 'js/main.js', 'node_modules/semantic-ui/dist/components/transition.js', 'node_modules/semantic-ui/dist/components/dimmer.js']))
         .pipe(concat('purified.css'))
         .pipe(gulp.dest('tmp/css'));
 });
@@ -120,7 +126,10 @@ gulp.task('pug', () => {
     gulp.src('src/pug/**/*.pug')
         .pipe(data(() => require('./src/data.json')))
         .pipe(data(() => {
-            return {products: require('./src/product.json')};
+            return {
+                products: require('./src/product.json'),
+                gallery: require('./src/photos.json')
+            };
         }))
         // .pipe(data(() => {
         //     let data = require('./src/data.json');
@@ -144,22 +153,66 @@ gulp.task('pug', () => {
 });
 
 
-gulp.task('thumbnails', () => {
-    gulp.src('img/photos/*')
+gulp.task('images', () => {
+    let imageminConfig = [
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ];
+
+    let streams = [];
+    let products = require('./src/product.json');
+    let productsThumbnails = Object.values(products).reduce((r, a) => [...r, ...a.images], []);
+    streams.push(gulp.src(productsThumbnails, {cwd: './img/photos/'})
         .pipe(imageresize({
             width: 300,
             height: 300,
             upscale: false,
-            crop: false,
+            crop: true,
             gravity: 'North'
         }))
-        .pipe(imagemin({
-            progressive: true,
-            // set this if you are using svg images
-            // svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
+        .pipe(imagemin(imageminConfig))
+        .pipe(gulp.dest('build/img/thumbnails')));
+
+    let photos = require('./src/photos.json');
+    let photosThumbnails = photos.reduce((r, photo) => [...r, photo.image], []);
+    streams.push(gulp.src(photosThumbnails, {cwd: './img/photos/'})
+        .pipe(imageresize({
+            height: 500,
+            upscale: false,
+            crop: false,
         }))
-        .pipe(gulp.dest('build/img/thumbnails'));
+        .pipe(imagemin(imageminConfig))
+        .pipe(gulp.dest('build/img/thumbnails')));
+
+    streams.push(gulp.src(['img/**/*.jpg', 'img/**/*.png', '!img/events/*.*'], {base: '.'})
+        .pipe(imageresize({
+            width: 1920,
+            height: 1080,
+            upscale: false,
+            crop: false,
+        }))
+        .pipe(imagemin(imageminConfig))
+        .pipe(gulp.dest('./build/')));
+
+    streams.push(gulp.src(['img/events/*.*'])
+        .pipe(imageresize({
+            height: 800,
+            upscale: false,
+            crop: false,
+        }))
+        .pipe(imagemin(imageminConfig))
+        .pipe(gulp.dest('build/img/events')));
+
+    let promises = streams.map(mkPromise);
+    Promise.all(promises).then(() => {
+        gulp.start('image-size');
+    });
 });
 
 gulp.task('image-size', () => {
@@ -171,12 +224,12 @@ gulp.task('image-size', () => {
 });
 
 gulp.task('copy', () => {
-    gulp.src('img/**').pipe(gulp.dest('build/img'));        // TODO: not just copy, but also optimise
     gulp.src('libs/photoswipe/dist/default-skin/*.*').pipe(gulp.dest('build/css'));
     gulp.src('libs/semantic/dist/themes/default/**').pipe(gulp.dest('build/css/themes/default'));
 });
 
 gulp.task('set-dev', dev.task);
+gulp.task('set-prod', prod.task);
 
-gulp.task('default', ['set-dev', 'connect', 'watch']);
-gulp.task('build', ['pug', /*'build-js',*/ 'build-css', 'copy', 'thumbnails']);
+gulp.task('default', sequence('set-dev', 'pug', 'build-css', 'connect', 'watch'));
+gulp.task('build', sequence('set-prod', 'pug', /*'build-js',*/ ['copy', 'images'], 'image-size', 'build-css'));
